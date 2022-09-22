ca3a1746626e673f41788f62c912e4056038e03f
Krzysztof Jakubik <krzysiek.jakubik@gmail.com>
2022-09-22 09:52:10 +0200

    chore: add watch scripts for all packages

--- packages/built/buildThemes.js
+++ packages/built/buildThemes.js
@@ -2,24 +2,34 @@ import { mkdirSync, existsSync, createWriteStream } from 'fs';
 import { join as joinPath } from 'path';
 import * as themes from '@ogcio-ds/themes';
 
-Object.entries(themes).forEach(async ([themeName, theme]) => {
-  const buildDirectory = joinPath(__dirname, 'tmp');
+try {
+  Object.entries(themes).forEach(async ([themeName, theme]) => {
+    try {
+      const buildDirectory = joinPath(__dirname, 'tmp');
 
-  if (!existsSync(buildDirectory)) {
-    mkdirSync(buildDirectory);
-  }
+      if (!existsSync(buildDirectory)) {
+        mkdirSync(buildDirectory);
+      }
 
-  const output = createWriteStream(
-    joinPath(buildDirectory, `${themeName}.scss`)
-  );
+      const output = createWriteStream(
+        joinPath(buildDirectory, `${themeName}.scss`)
+      );
 
-  output.write(':root {\n');
-  Object.entries(theme).forEach(([key, value]) => {
-    output.write(`  ${key}: ${value};\n`);
+      output.write(':root {\n');
+      Object.entries(theme).forEach(([key, value]) => {
+        output.write(`  ${key}: ${value};\n`);
+      });
+      output.write('}\n');
+
+      output.write('@import "../src/theme";\n');
+
+      output.close();
+
+      console.log(`✨ Theme "${themeName}" built successfully`);
+    } catch (error) {
+      console.error(`❌ Failed building theme "${themeName}"`, error);
+    }
   });
-  output.write('}\n');
-
-  output.write('@import "../src/theme";\n');
-
-  output.close();
-});
+} catch (error) {
+  console.error('❌ Failed building themes', error);
+}
