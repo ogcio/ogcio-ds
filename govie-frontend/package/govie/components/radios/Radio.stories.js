320facda31fa43c4762329f51c50c4c2b8db6d43
Ana Lemos <ana.lemos@nearform.com>
2022-11-11 12:21:54 -0300

    feat: add radio with hint

--- govie-frontend/package/govie/components/radios/Radio.stories.js
+++ govie-frontend/package/govie/components/radios/Radio.stories.js
@@ -11,16 +11,21 @@ export default {
   argTypes: {
     id: { control: 'text' },
     label: { control: 'text' },
+    generalHint: { control: 'text' },
     options: { control: 'text' },
+    inline: { control: 'boolean' },
+    title: { control: 'boolean' }
   },
   args: {
     inline: false,
+    title: true
   },
 };
 
-const createLegend = (label) => {
+const createLegend = ({ label, title}) => {
   const legend = document.createElement('legend');
-  legend.className = 'govie-fieldset__legend govie-fieldset__legend--l';
+  const large = title ? 'govie-fieldset__legend--l' : '';
+  legend.className = ['govie-fieldset__legend', large].join(' ');
 
   const h1 = document.createElement('h1');
   h1.className = 'govie-fieldset__heading';
@@ -60,10 +65,24 @@ const Template = (args) => {
   const fieldset = document.createElement('fieldset');
   fieldset.className = 'govie-fieldset';
 
-  container.appendChild(createLegend(args.label));
+  container.appendChild(createLegend({
+    label: args.label, 
+    title: args.title
+  }));
+
+  if (args.generalHint) {
+    const hint = document.createElement('div');
+    hint.id = args.id;
+    hint.className = 'govie-hint';
+    hint.innerText = args.generalHint;
+
+    container.appendChild(hint);
+  }
 
   const group = document.createElement('div');
-  group.className = 'govie-radios';
+
+  const inline = args.inline ? 'govie-radios--inline' : '';
+  group.className = ['govie-radios', inline].join(' ');
   group.setAttribute('data-module', 'govie-radios');
 
   args.options?.split(',').forEach((label, index) => {
@@ -87,4 +106,28 @@ Radio.args = {
   id: 'where-do-you-live',
   label: 'Where do you live?',
   options: 'England,Scottland,Ireland'
+};
+
+export const SubheadingRadio = Template.bind({});
+SubheadingRadio.args = {
+  id: 'where-do-you-live',
+  label: 'Where do you live?',
+  options: 'England,Scottland,Ireland',
+  title: false
+};
+
+export const InlineRadio = Template.bind({});
+InlineRadio.args = {
+  id: 'changed-name',
+  label: 'Have you changed your name?',
+  options: 'Yes,No',
+  inline: true
+};
+
+export const RadioWithHints = Template.bind({});
+RadioWithHints.args = {
+  id: 'sign-in',
+  label: 'How do you want to sign in?',
+  options: 'Sign in with Gateway,Sign in with Verify',
+  generalHint: 'You\'ll need an account to prove your identity and complete your Self Assessment.'
 };
