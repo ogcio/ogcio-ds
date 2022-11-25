4d6bee6f7e0da1a6143bbeeb3fb5803e7654531b
Dénes Fekésházy <denes.fekeshazy@nearform.com>
2022-11-25 16:06:35 +0000

    5391: add stories for tag component

---
+++ govie-frontend/package/govie/components/tag/Tag.stories.js
@@ -0,0 +1,108 @@
+export default {
+  title: 'Typography/Tag',
+  parameters: {
+    docs: {
+      description: {
+        component:
+          'Use the tag component when it’s possible for something to have more than one status and it’s useful for the user to know about that status.',
+      },
+    },
+  },
+  argTypes: {
+    text: {
+      control: 'text',
+      type: { name: 'string', required: true },
+    },
+    type: {
+      control: 'radio',
+      options: [
+        'default',
+        'grey',
+        'green',
+        'turquoise',
+        'blue',
+        'purple',
+        'pink',
+        'red',
+        'orange',
+        'yellow',
+      ],
+    },
+  },
+  args: {
+    type: 'default',
+  },
+}
+
+const Template = (args) => {
+  const classNames = ['govie-tag']
+  if (args.type && args.type.toLowerCase() !== 'default') {
+    classNames.push(`govie-tag--${args.type}`)
+  }
+
+  const tag = document.createElement('strong')
+  tag.className = classNames.join(' ')
+  tag.innerText = args.text
+
+  return tag
+}
+
+export const Default = Template.bind({})
+Default.args = {
+  text: 'Completed',
+  type: 'default',
+}
+
+export const Grey = Template.bind({})
+Grey.args = {
+  text: 'Inactive',
+  type: 'grey',
+}
+
+export const Green = Template.bind({})
+Green.args = {
+  text: 'New',
+  type: 'green',
+}
+
+export const Turquoise = Template.bind({})
+Turquoise.args = {
+  text: 'Active',
+  type: 'turquoise',
+}
+
+export const Blue = Template.bind({})
+Blue.args = {
+  text: 'Pending',
+  type: 'blue',
+}
+
+export const Purple = Template.bind({})
+Purple.args = {
+  text: 'Received',
+  type: 'purple',
+}
+
+export const Pink = Template.bind({})
+Pink.args = {
+  text: 'Sent',
+  type: 'pink',
+}
+
+export const Red = Template.bind({})
+Red.args = {
+  text: 'Rejected',
+  type: 'red',
+}
+
+export const Orange = Template.bind({})
+Orange.args = {
+  text: 'Declined',
+  type: 'orange',
+}
+
+export const Yellow = Template.bind({})
+Yellow.args = {
+  text: 'Delayed',
+  type: 'yellow',
+}
