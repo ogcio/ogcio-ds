0183f791994042556f09c23d193081ac0c1c791a
Dénes Fekésházy <denes.fekeshazy@nearform.com>
2022-11-25 12:02:51 +0000

    6113: refactor HTML formatting to support tables

---
+++ govie-frontend/.storybook/helpers/beautifyHtmlNode.js
@@ -0,0 +1,16 @@
+import { html as beautify } from 'js-beautify'
+
+const beautifyHtmlNode = (node) => {
+  const beautifyOptions = {
+    indent_size: 2,
+    end_with_newline: false,
+    preserve_newlines: false,
+    inline: ['inline'],
+  }
+
+  const formattedNode = beautify(node.outerHTML, beautifyOptions)
+
+  return formattedNode
+}
+
+export default beautifyHtmlNode
