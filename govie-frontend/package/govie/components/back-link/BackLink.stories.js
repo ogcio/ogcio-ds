0317b9b3f9cb1ff5b538149403ff581e3e43d97c
Dénes Fekésházy <denes.fekeshazy@nearform.com>
2022-11-25 16:28:25 +0000

    6115: add BackLink component to storybook

---
+++ govie-frontend/package/govie/components/back-link/BackLink.stories.js
@@ -0,0 +1,32 @@
+export default {
+  title: 'Typography/Back link',
+  parameters: {
+    docs: {
+      description: {
+        component:
+          'Use the back link component to help users go back to the previous page in a multi-page transaction.',
+      },
+    },
+  },
+  argTypes: {
+    url: {
+      control: 'text',
+      type: { name: 'string', required: true },
+    },
+  },
+  args: {
+    url: '#',
+  },
+}
+
+const Template = (args) => {
+  const backLink = document.createElement('a')
+  backLink.href = args.url
+  backLink.className = 'govie-back-link'
+  backLink.innerText = 'Back'
+
+  return backLink
+}
+
+export const Default = Template.bind({})
+Default.args = {}
