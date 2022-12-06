0d41a5670226b192e2ccff4ea0436dec902516d4
Ana Lemos <ana.lemos@nearform.com>
2022-12-06 16:30:47 -0300

    feat: add colours documentation page

--- govie-frontend/.storybook/preview.js
+++ govie-frontend/.storybook/preview.js
@@ -1,3 +1,4 @@
+import React from 'react'
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
@@ -8,7 +9,36 @@ export const parameters = {
     },
     sort: 'requiredFirst'
   },
-  docs: { source: { format: false } },
+  docs: {
+    source: { format: false },
+    components: {
+      a: ({ children, ...args }) => (
+        <a className="govie-link" {...args}>
+          {children}
+        </a>
+      ),
+      p: ({ children, ...args }) => (
+        <p className="govie-body" {...args}>
+          {children}
+        </p>
+      ),
+      h1: ({ children, ...args }) => (
+        <h1 className="govie-heading-l" {...args}>
+          {children}
+        </h1>
+      ),
+      h2: ({ children, ...args }) => (
+        <h2 className="govie-heading-m" {...args}>
+          {children}
+        </h2>
+      ),
+      h3: ({ children, ...args }) => (
+        <h2 className="govie-heading-s" {...args}>
+          {children}
+        </h2>
+      )
+    }
+  },
   options: {
     storySort: {
       order: ['Docs', 'Form', 'Typography', 'Navigation', 'Page & Layout'],
