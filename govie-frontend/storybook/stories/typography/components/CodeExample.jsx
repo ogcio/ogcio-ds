24c8c2e1632670f2946713862561eb3492bd7b5f
Ana Lemos <ana.lemos@nearform.com>
2023-01-13 14:48:12 -0300

    feat: add typography doc page

---
+++ govie-frontend/storybook/stories/typography/components/CodeExample.jsx
@@ -0,0 +1,30 @@
+/* eslint-disable no-unused-vars */
+import { CopyBlock, atomOneLight } from 'react-code-blocks'
+import React from 'react'
+
+export const CodeExample = ({ code, darkMode }) => (
+  <div
+    style={{
+      border: '1px solid',
+      marginBottom: '20px',
+      borderRadius: '4px',
+    }}
+  >
+    <div
+      dangerouslySetInnerHTML={{ __html: code }}
+      style={{
+        padding: '16px',
+        borderBottom: '1px solid',
+        backgroundColor: darkMode ? '#1d70b8' : 'transparent',
+      }}
+    />
+    <CopyBlock
+      customStyle={{ fontSize: '18px', padding: '16px' }}
+      text={code}
+      language="js"
+      showLineNumbers={false}
+      theme={atomOneLight}
+      wrapLines
+    />
+  </div>
+)
