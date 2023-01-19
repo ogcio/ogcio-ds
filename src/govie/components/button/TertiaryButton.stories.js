a224d885df43689d2d031eb17023c64cfb9a5ee3
Ana Lemos <ana.lemos@nearform.com>
2023-01-19 12:29:38 -0300

    feat: add tertiary button

---
+++ src/govie/components/button/TertiaryButton.stories.js
@@ -0,0 +1,50 @@
+import beautifyHtmlNode from '../../../../.storybook/helpers/beautifyHtmlNode'
+import parseHtmlString from '../../../../.storybook/helpers/parseHtmlString'
+import primary, { Default as button } from './PrimaryButton.stories'
+
+export default {
+  ...primary,
+  title: 'Form/Button/Tertiary button',
+}
+
+const Template = (args) => {
+  const btn = parseHtmlString(button(args))
+  btn.className = `${btn.classList} govie-button--tertiary`
+
+  return beautifyHtmlNode(btn)
+}
+
+export const Default = Template.bind({})
+Default.args = {
+  label: 'Tertiary button',
+}
+
+export const Medium = Template.bind({})
+Medium.args = {
+  label: 'Medium tertiary button',
+  size: 'medium',
+}
+
+export const Small = Template.bind({})
+Small.args = {
+  label: 'Small tertiary button',
+  size: 'small',
+}
+
+export const WithLeftIcon = Template.bind({})
+WithLeftIcon.args = {
+  label: 'Tertiary button with left icon',
+  icon: 'left',
+}
+
+export const WithRightIcon = Template.bind({})
+WithRightIcon.args = {
+  label: 'Tertiary button with right icon',
+  icon: 'right',
+}
+
+export const Disabled = Template.bind({})
+Disabled.args = {
+  label: 'Disabled tertiary button',
+  disabled: true,
+}
