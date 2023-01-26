12a54ac80a5208c33baddc6b643c512498eba08a
Ana Lemos <anaapaulalemos@gmail.com>
2023-01-26 11:57:46 -0300

    feat: add tick component (#92)
    * feat: add tick component
    
    * chore: update storybook dist minified files
    
    * fix: replace copy & paste code
    
    * refactor: rename the style filenames to match the standard

---
+++ src/govie/components/tick/Tick.stories.js
@@ -0,0 +1,54 @@
+import beautifyHtmlNode from '../../../../.storybook/helpers/beautifyHtmlNode'
+
+export default {
+  title: 'Application/Tick',
+  parameters: {
+    docs: {
+      description: {
+        component:
+          'Ticks toggle the state of a single setting active or inactive.',
+      },
+    },
+  },
+  argTypes: {
+    filled: { control: 'boolean' },
+    value: {
+      control: 'text',
+      type: { name: 'text', required: true },
+    },
+  },
+  args: {
+    filled: false,
+  },
+}
+
+const Template = (args) => {
+  const input = document.createElement('input')
+
+  const classNames = ['govie-tick']
+
+  if (args.filled) {
+    classNames.push('govie-tick--filled')
+  }
+
+  input.className = classNames.join(' ')
+  input.type = 'checkbox'
+  input.value = args.value
+
+  const label = document.createElement('label')
+  label.setAttribute('data-module', 'govie-tick')
+  label.appendChild(input)
+
+  return beautifyHtmlNode(label)
+}
+
+export const Default = Template.bind({})
+Default.args = {
+  value: 'checkbox'
+}
+
+export const Filled = Template.bind({})
+Filled.args = {
+  value: 'checkbox',
+  filled: true,
+}
