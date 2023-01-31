d4e713fcd2c14857c99327afa8ec2728b4351a5d
Ana Lemos <ana.lemos@nearform.com>
2023-01-31 14:38:23 -0300

    feat: add storybook docs

--- src/govie/components/icon-button/IconButton.stories.js
+++ src/govie/components/icon-button/IconButton.stories.js
@@ -26,7 +26,7 @@ const Template = (args) => {
 
   btn.innerHTML = `
     <svg width="14" height="15" viewBox="0 0 14 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M14 8.5H8V14.5H6V8.5H0V6.5H6V0.5H8V6.5H14V8.5Z" fill="#505A5F"/>
+        <path d="M14 8.5H8V14.5H6V8.5H0V6.5H6V0.5H8V6.5H14V8.5Z" />
     </svg>
     `
 
