588b9d5c90f2480472ed92cef09bfe5e244c6206
Dénes Fekésházy <denes.fekeshazy@nearform.com>
2023-03-20 15:30:05 +0000

    7396: fix typo and refactor Modal component

--- src/govie/components/modal/Modal.stories.js
+++ src/govie/components/modal/Modal.stories.js
@@ -28,54 +28,79 @@ export default {
   },
 }
 
-const Template = (args) => {
-  const container = document.createElement('div')
-  container.className = 'govie-modal'
+const createModalOverlay = () => {
+  const overlay = document.createElement('div')
+  overlay.className = 'govie-modal--overlay'
 
-  const title = parseHtmlString(
-    heading({ text: 'This is a semantic title', size: 's' })
-  )
-  container.appendChild(title)
+  return overlay
+}
 
-  const firstParagraph = parseHtmlString(
-    paragraph({
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    })
-  )
-  container.appendChild(firstParagraph)
-
-  const secondParagraph = parseHtmlString(
-    paragraph({
-      text: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    })
-  )
-  container.appendChild(secondParagraph)
-
-  const buttonsRow = document.createElement('div')
-  buttonsRow.className = 'govie-modal--buttons'
-
-  const canelButton = parseHtmlString(
-    outlinedButton({ label: 'Cancel Action', size: 'medium' })
-  )
-  buttonsRow.appendChild(canelButton)
-
-  const confirmButton = parseHtmlString(
-    primaryButton({ label: 'Primary Action', size: 'medium' })
-  )
-  buttonsRow.appendChild(confirmButton)
-
-  container.appendChild(buttonsRow)
-
+const createCloseButton = () => {
   const closeButtonContainer = document.createElement('div')
   closeButtonContainer.className = 'govie-modal--close-button-container'
 
   const xIconSvg = `<svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
   <path d="M14 1.41L12.59 0L7 5.59L1.41 0L0 1.41L5.59 7L0 12.59L1.41 14L7 8.41L12.59 14L14 12.59L8.41 7L14 1.41Z" fill="#505A5F"/>
 </svg>`
-  const closeButton = parseHtmlString(iconButton({ icon: xIconSvg }))
+  const closeButton = parseHtmlString(
+    iconButton({ icon: xIconSvg, withoutTooltip: true })
+  )
   closeButtonContainer.appendChild(closeButton)
 
-  container.appendChild(closeButtonContainer)
+  return closeButtonContainer
+}
+
+const createContentButtonsRow = () => {
+  const buttonsRow = document.createElement('div')
+  buttonsRow.className = 'govie-modal--buttons'
+
+  const cancelButton = parseHtmlString(
+    outlinedButton({ label: 'Cancel Action', size: 'medium' })
+  )
+  buttonsRow.appendChild(cancelButton)
+
+  const confirmButton = parseHtmlString(
+    primaryButton({ label: 'Primary Action', size: 'medium' })
+  )
+  buttonsRow.appendChild(confirmButton)
+
+  return buttonsRow
+}
+
+const createModalContent = () => {
+  const content = document.createElement('div')
+  content.className = 'govie-modal--content'
+  content.appendChild(createCloseButton())
+
+  const title = parseHtmlString(
+    heading({ text: 'This is a semantic title', size: 's' })
+  )
+  content.appendChild(title)
+
+  const firstParagraph = parseHtmlString(
+    paragraph({
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    })
+  )
+  content.appendChild(firstParagraph)
+
+  const secondParagraph = parseHtmlString(
+    paragraph({
+      text: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    })
+  )
+  content.appendChild(secondParagraph)
+
+  content.appendChild(createContentButtonsRow())
+
+  return content
+}
+
+const Template = (args) => {
+  const container = document.createElement('div')
+  container.className = 'govie-modal'
+  container.appendChild(createModalOverlay())
+  container.appendChild(createModalContent())
 
   return beautifyHtmlNode(container)
 }
