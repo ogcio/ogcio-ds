e1a423ca8651a5991beb3e1efed05cc1cb848300
hamza14khan <hamzaubit14@gmail.com>
2024-04-29 09:19:46 +0100

    fix: minor refactoring to the code and updated dist

--- postcss.config.js
+++ postcss.config.js
@@ -17,20 +17,20 @@ import unrgba from 'postcss-unrgba';
  * @returns {import('postcss-load-config').Config} PostCSS config
  */
 export default (ctx) => {
-  const plugins = [];
-  const syntax = ctx.to?.endsWith('.scss') ? scss : postcss;
+  const plugins = []
+  const syntax = ctx.to?.endsWith('.scss') ? scss : postcss
 
   // PostCSS 'from' (or webpack 'file') source path
   // https://github.com/postcss/postcss-load-config#options
-  const { dir, name } = parse(ctx.from || ctx.file || '');
+  const { dir, name } = parse(ctx.from || ctx.file || '')
 
   // IE8 stylesheets
-  const isIE8 = name?.endsWith('-ie8') || name?.endsWith('-ie8.min');
+  const isIE8 = name?.endsWith('-ie8') || name?.endsWith('-ie8.min')
 
   // Add vendor prefixes
   plugins.push(autoprefixer({
     env: isIE8 ? 'oldie' : ctx.env
-  }));
+  }))
 
   // Add review app auto-generated 'companion' classes for each pseudo-class
   // For example ':hover' and ':focus' classes to simulate form label states
@@ -44,7 +44,7 @@ export default (ctx) => {
         ':active',
         ':focus'
       ]
-    }));
+    }))
   }
 
   // Transpile CSS for Internet Explorer
@@ -53,7 +53,7 @@ export default (ctx) => {
       unmq(),
       unopacity({ browsers: 'ie 8' }),
       unrgba({ filter: true })
-    );
+    )
   }
 
   // Always minify CSS
@@ -64,11 +64,11 @@ export default (ctx) => {
         // https://cssnano.co/docs/optimisations/cssdeclarationsorter/
         cssDeclarationSorter: false
       }]
-    }));
+    }))
   }
 
   return {
     syntax,
     plugins
-  };
-};
+  }
+}
