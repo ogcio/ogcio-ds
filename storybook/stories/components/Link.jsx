0d48d125bc4757e750164bc219a137d969dd7fe3
hamza14khan <hamzaubit14@gmail.com>
2024-06-19 13:54:10 +0100

    docs: repo link update and minor adjustments to docs

--- storybook/stories/components/Link.jsx
+++ storybook/stories/components/Link.jsx
@@ -1,5 +1,13 @@
+import packageJson from '../../../package.json';
 import React from 'react'
 
 export const Link = ({ children, href }) => (
 	<a className='govie-link' href={`${import.meta.env.BASE_URL}${href}`}>{children}</a>
 );
+
+export const ReleaseLink = ({ children }) => {
+  const version =  packageJson.version;
+  return (
+    <a className='govie-link' href={`https://github.com/ogcio/ogcio-ds/releases/tag/v${version}`}>{children}</a>
+  )
+}
