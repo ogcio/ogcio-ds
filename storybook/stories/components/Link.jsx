6e57a23cb37631c6636e0212179a0aaa7483d196
hamza14khan <hamzaubit14@gmail.com>
2024-06-20 14:56:10 +0100

    fix: prettier with jsx and associated formatting issues

--- storybook/stories/components/Link.jsx
+++ storybook/stories/components/Link.jsx
@@ -1,13 +1,20 @@
 import packageJson from '../../../package.json';
-import React from 'react'
+import React from 'react';
 
 export const Link = ({ children, href }) => (
-	<a className='govie-link' href={`${import.meta.env.BASE_URL}${href}`}>{children}</a>
+  <a className="govie-link" href={`${import.meta.env.BASE_URL}${href}`}>
+    {children}
+  </a>
 );
 
 export const ReleaseLink = ({ children }) => {
-  const version =  packageJson.version;
+  const version = packageJson.version;
   return (
-    <a className='govie-link' href={`https://github.com/ogcio/ogcio-ds/releases/tag/v${version}`}>{children}</a>
-  )
-}
+    <a
+      className="govie-link"
+      href={`https://github.com/ogcio/ogcio-ds/releases/tag/v${version}`}
+    >
+      {children}
+    </a>
+  );
+};
