654a3a0f57af4c6fb1f555cc9f407b9cac492415
William Monteiro <william.wd@gmail.com>
2024-08-13 14:41:59 -0300

    Fix ACLs not being created by Terraform

--- infra/terraform/s3/s3.tf
+++ infra/terraform/s3/s3.tf
@@ -2,9 +2,17 @@ resource "aws_s3_bucket" "bucket" {
   bucket = var.s3_bucket_name
 }
 
-resource "aws_s3_bucket_acl" "bucket_acl" {
+resource "aws_s3_bucket_ownership_controls" "bucket_controls" {
   bucket = aws_s3_bucket.bucket.id
-  acl    = var.s3_bucket_acl
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  depends_on = [aws_s3_bucket_ownership_controls.bucket_controls]
+  bucket     = aws_s3_bucket.bucket.id
+  acl        = var.s3_bucket_acl
 }
 
 resource "aws_s3_bucket_versioning" "bucket_versioning" {
