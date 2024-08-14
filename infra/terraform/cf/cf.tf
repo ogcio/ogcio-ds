b210a9a350b42a829c679e85f34d6acf87839b1d
William Monteiro <william.wd@gmail.com>
2024-08-14 12:08:08 -0300

    Update minimum TLS protocol, which is a security concern

--- infra/terraform/cf/cf.tf
+++ infra/terraform/cf/cf.tf
@@ -51,8 +51,9 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   aliases = var.cf_aliases
 
   viewer_certificate {
-    acm_certificate_arn = var.cf_viewer_certificate_arn
-    ssl_support_method  = var.cf_viewer_certificate_ssl_support_method
+    acm_certificate_arn      = var.cf_viewer_certificate_arn
+    ssl_support_method       = var.cf_viewer_certificate_ssl_support_method
+    minimum_protocol_version = "TLSv1.2_2021"
   }
 
   restrictions {
